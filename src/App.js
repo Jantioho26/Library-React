f031d1889456056c53118f35140befbd7cdd755d
Joy Antioho <j.wildfong@yahoo.com>
2026-08-05 15:46:25 -0400

    Library-React

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Landing from './components/Landing';
 import Nav from './components/Nav';
 import Highlights from './components/Highlights';
 import Featured from './components/Featured';
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
      <Landing />
      <Highlights />
      <Featured />
+     <Discounted />
+     <Explore />
+     <Footer />
     </div>
   );
 }
