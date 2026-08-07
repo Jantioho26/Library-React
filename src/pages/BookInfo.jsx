f5ede64bc51a35bf27b8ee9cc23df567fd649ba1
Joy Antioho <j.wildfong@yahoo.com>
2026-08-07 07:15:17 -0400

    Library-React

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 const BookInfo = ({ books }) => {
   return (
     <div id="books__body">
       <main id="books__main">
-        <div className="row">
-          <div className="book_-selected--top"></div>
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top"></div>
+          </div>
         </div>
       </main>
     </div>
